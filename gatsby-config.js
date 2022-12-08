549e8e3d153da04ecbf2fb9ff81b4c0cb7594fd4
Rachelle Ragasa <rachelleragasa.costa@gmail.com>
2022-12-08 17:11:27 +0000

    creating gatsby site

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `rsod`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
